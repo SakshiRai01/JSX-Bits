e15c30a571508713ed591ccc9b0375994866f85b
SakshiRai01 <0111sakshirai@gmail.com>
2024-04-25 21:20:44 +0530

    New repository initial commit

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import Button from './Button';
+
+function App() {
+  return (
+    <div>
+      <div>
+        <Button>Click me!!</Button>
+      </div>
+      <div>
+        <Button>Buy now!</Button>
+      </div>
+      <div>
+        <Button>See Deal!</Button>
+      </div>
+      <div>
+        <Button>Hey there!</Button>
+      </div>
+      <div>
+        <Button>Welcome!</Button>
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Button.js
@@ -0,0 +1,10 @@
+function Button({children})
+{
+    return (
+        <button>
+            {children}
+        </button>
+    );
+}
+
+export default Button;
