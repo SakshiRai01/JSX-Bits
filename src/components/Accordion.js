2626233f3ceb320a8760336e2c290d3c8bbc9e95
SakshiRai01 <0111sakshirai@gmail.com>
2024-04-30 22:49:36 +0530

    New page Accordion

---
+++ src/components/Accordion.js
@@ -0,0 +1,19 @@
+function Accordion({ items }) {
+    const renderedItems = items.map((item) => {
+        return (
+            <div key={item.id}>
+                <div>
+                    {item.label}
+                </div>
+                <div>
+                    {item.content}
+                </div>
+            </div>
+        );
+    });
+    return <div>
+        {renderedItems}
+    </div>;
+}
+
+export default Accordion;
