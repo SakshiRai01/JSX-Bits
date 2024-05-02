957e3ca7ce0a763fb3e6e6c62f6362b038d44eb7
SakshiRai01 <0111sakshirai@gmail.com>
2024-05-02 19:02:21 +0530

    Event Handler

--- src/components/Accordion.js
+++ src/components/Accordion.js
@@ -3,16 +3,21 @@ import { useState } from 'react';
 function Accordion({ items }) {
     const [expandedIndex, setExpandedIndex] = useState(0);
 
+    const handleClick = (nextIndex) => {
+        setExpandedIndex(nextIndex);
+    }
+
     const renderedItems = items.map((item, index) => {
         const isExpanded = index === expandedIndex;
 
         return (
             <div key={item.id}>
-                <div onClick={() => setExpandedIndex(index)}>{item.label}</div>
+                <div onClick={() => handleClick(index)}>{item.label}</div>
                 {isExpanded && <div>{item.content}</div>}
             </div>
         );
     });
+
     return <div>
         {renderedItems}
     </div>;
