5175dc821512f9a78da8a0c18fd46d846f535f65
SakshiRai01 <0111sakshirai@gmail.com>
2024-05-02 19:20:58 +0530

    Added Tailwind CSS

--- src/components/Accordion.js
+++ src/components/Accordion.js
@@ -11,22 +11,22 @@ function Accordion({ items }) {
     const renderedItems = items.map((item, index) => {
         const isExpanded = index === expandedIndex;
 
-        const icon = <span>
-            {isExpanded ? <HiChevronDown/> : <HiChevronLeft/>}
+        const icon = <span className="text-2xl">
+            {isExpanded ? <HiChevronDown /> : <HiChevronLeft />}
         </span>
 
         return (
             <div key={item.id}>
-                <div onClick={() => handleClick(index)}>
-                    {icon}
+                <div className="flex justify-between p-3 bg-gray-50 border-b items-center cursor-pointer" onClick={() => handleClick(index)}>
                     {item.label}
+                    {icon}
                 </div>
-                {isExpanded && <div>{item.content}</div>}
+                {isExpanded && <div className="border-b p-5">{item.content}</div>}
             </div>
         );
     });
 
-    return <div>
+    return <div className="border-x border-t rounded">
         {renderedItems}
     </div>;
 }
