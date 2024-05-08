d7fbfddc2a0505fb92d4a695238da08c6a0e41ae
SakshiRai01 <0111sakshirai@gmail.com>
2024-05-08 19:32:24 +0530

    styling Tailwind CSS and react-icons

--- src/components/Dropdown.js
+++ src/components/Dropdown.js
@@ -1,4 +1,5 @@
 import { useState } from 'react';
+import { IoIosArrowDropdown } from "react-icons/io";
 
 function Dropdown({ options , selection , onSelect }) {
     const [isOpen, setIsOpen] = useState(false);
@@ -13,20 +14,27 @@ function Dropdown({ options , selection , onSelect }) {
     }
 
     const renderedOptions = options.map((option) => {
-        return <div onClick={() => handleOptionClick(option)} key={option.value}>
+        return <div className="hover:bg-sky-100 rounded cursor-pointer p-1" onClick={() => handleOptionClick(option)} key={option.value}>
             {option.label}
         </div>
     });
 
-    let content = 'Select..';
+    let content = 'Select a color';
     if (selection)
     {
         content = selection.label;
     }
 
-    return <div>
-        <div onClick={handleClick}>{content}</div>
-        {isOpen && <div>{renderedOptions}</div>}
+    return <div className="w-48 relative">
+        <div className="flex justify-between items-centre cursor-pointer border rounded p-3 shadow bg-white w-full"
+            onClick={handleClick}>
+            {content}
+            <IoIosArrowDropdown className="text-lg" />
+        </div>
+
+        {isOpen && <div className='absolute top-full border rounded p-3 shadow bg-white w-full'>{renderedOptions}
+        </div>}
+        
     </div>
 }
 
